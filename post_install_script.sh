b659d61cb8c2f17e7280bb89370edeb5fb59b12c
KnightTheCoder <knight14animator@gmail.com>
2024-01-15 22:34:19 +0100

    Switched to automatic install

--- post_install_script.sh
+++ post_install_script.sh
@@ -13,9 +13,9 @@ echo "Refreshing repositories..."
 sudo zypper ref
 
 echo "Upgrading system..."
-sudo zypper -v dup
+sudo zypper -v -y dup
 
-echo "Check the internet..."
+echo "Checking the internet connection..."
 wget -q --spider http://google.com
 
 if [ $? -eq 0 ]; then
@@ -28,20 +28,20 @@ fi
 
 echo "Installing codecs..."
 sudo zypper ar -cfp 90 https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ packman
-sudo zypper -v dup --from packman --allow-vendor-change
+sudo zypper -v -y dup --from packman --allow-vendor-change
 
-sudo zypper -v install opi
+sudo zypper -v -y install opi
 opi codecs
 
 echo "Removing unnecessary packages and installing extra ones..."
-sudo zypper -v rm --clean-deps discover kmail kontact kmines akregator kaddressbook korganizer kompare konversation tigervnc kleopatra kmahjongg kpat kreversi ksudoku
-sudo zypper -v in fish neofetch htop kwrite
+sudo zypper -v -y rm --clean-deps discover kmail kontact kmines akregator kaddressbook korganizer kompare konversation tigervnc kleopatra kmahjongg kpat kreversi ksudoku
+sudo zypper -v -y in fish neofetch htop kwrite
 
 echo "Installing build tools..."
-sudo zypper -v in -t pattern devel_basis
+sudo zypper -v -y in -t pattern devel_basis
 
 echo "Installing microsoft fonts..."
-sudo zypper -v in fetchmsttfonts
+sudo zypper -v -y in fetchmsttfonts
 
 echo "Installing oh my fish!..."
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
@@ -50,19 +50,19 @@ echo "Copying fish config..."
 cp ./config.fish ~/.config/fish/config.fish -vf
 
 echo "Installing gaming and other extra apps..."
-sudo zypper in -v lutris goverlay mangohud transmission-gtk haruna celluloid strawberry
+sudo zypper in -v -y lutris goverlay mangohud transmission-gtk haruna celluloid strawberry
 
 echo "Installing visual studio code..."
 sudo zypper ar obs://devel:tools:ide:vscode devel_tools_ide_vscode
-sudo zypper -v in code
+sudo zypper -v -y in code
 
 echo "Configuring flatpak and installing flatpak apps..."
-sudo zypper -v in flatpak
+sudo zypper -v -y in flatpak
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo usermod -a -G wheel $USER
 
-flatpak install io.missioncenter.MissionCenter com.github.tchx84.Flatseal org.gimp.GIMP org.kde.kdenlive com.valvesoftware.Steam net.davidotek.pupgui2 com.obsproject.Studio com.github.unrud.VideoDownloader
+flatpak install -y io.missioncenter.MissionCenter com.github.tchx84.Flatseal org.gimp.GIMP org.kde.kdenlive com.valvesoftware.Steam net.davidotek.pupgui2 com.obsproject.Studio com.github.unrud.VideoDownloader
 
 echo "Post install complete, enjoy your new distro!"
 echo "Please run 'fish && omf install bobthefish' to install the omf theme"
