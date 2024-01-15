a5db421a8551a5e870904e6bd954e77cae896a8f
KnightTheCoder <knight14animator@gmail.com>
2024-01-15 22:58:17 +0100

    assume yes position corrected

--- post_install_script.sh
+++ post_install_script.sh
@@ -13,7 +13,7 @@ echo "Refreshing repositories..."
 sudo zypper ref
 
 echo "Upgrading system..."
-sudo zypper -v -y dup
+sudo zypper -v dup -y
 
 echo "Checking the internet connection..."
 wget -q --spider http://google.com
@@ -28,20 +28,20 @@ fi
 
 echo "Installing codecs..."
 sudo zypper ar -cfp 90 https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ packman
-sudo zypper -v -y dup --from packman --allow-vendor-change
+sudo zypper -v dup -y --from packman --allow-vendor-change
 
-sudo zypper -v -y install opi
+sudo zypper -v in opi -y
 opi codecs
 
 echo "Removing unnecessary packages and installing extra ones..."
-sudo zypper -v -y rm --clean-deps discover kmail kontact kmines akregator kaddressbook korganizer kompare konversation tigervnc kleopatra kmahjongg kpat kreversi ksudoku
-sudo zypper -v -y in fish neofetch htop kwrite
+sudo zypper -v rm -y --clean-deps discover kmail kontact kmines akregator kaddressbook korganizer kompare konversation tigervnc kleopatra kmahjongg kpat kreversi ksudoku
+sudo zypper -v in -y fish neofetch htop kwrite
 
 echo "Installing build tools..."
-sudo zypper -v -y in -t pattern devel_basis
+sudo zypper -v in -y -t pattern devel_basis
 
 echo "Installing microsoft fonts..."
-sudo zypper -v -y in fetchmsttfonts
+sudo zypper -v in -y fetchmsttfonts
 
 echo "Installing oh my fish!..."
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
@@ -50,14 +50,14 @@ echo "Copying fish config..."
 cp ./config.fish ~/.config/fish/config.fish -vf
 
 echo "Installing gaming and other extra apps..."
-sudo zypper -v -y in lutris goverlay mangohud transmission-gtk haruna celluloid strawberry
+sudo zypper -v in -y lutris goverlay mangohud transmission-gtk haruna celluloid strawberry
 
 echo "Installing visual studio code..."
 sudo zypper ar obs://devel:tools:ide:vscode devel_tools_ide_vscode
-sudo zypper -v -y in code
+sudo zypper -v in -y code
 
 echo "Configuring flatpak and installing flatpak apps..."
-sudo zypper -v -y in flatpak
+sudo zypper -v in -y flatpak
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo usermod -a -G wheel $USER
