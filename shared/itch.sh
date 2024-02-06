4ac3102c2eec873f56e494b90564cd9b025c2c1e
KnightTheCoder <knight14animator@gmail.com>
2024-02-06 23:23:35 +0100

    Itch won't reinstall and typo fix in readme

--- shared/itch.sh
+++ shared/itch.sh
@@ -2,6 +2,15 @@
 
 cd "$(dirname "$0")" || exit
 
+# spellcheck source=../shared/colors.sh
+source "../shared/colors.sh"
+
+# Check if itch is installed
+if [ -x "$HOME/.itch/itch" ]; then
+    echo -e "${YELLOW}Itch desktop app is already installed!${NC}"
+    exit
+fi
+
 wget -O itch-setup "https://itch.io/app/download?platform=linux"
 chmod +x "./itch-setup"
 ./itch-setup
