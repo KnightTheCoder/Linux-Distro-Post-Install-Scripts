e2209250801ee63b732653dd3ee12f751e9f92da
KnightTheCoder <knight14animator@gmail.com>
2024-02-22 15:27:34 +0100

    fedira refresg for vscide

--- distros/fedora/setup.sh
+++ distros/fedora/setup.sh
@@ -125,7 +125,7 @@ for app in "${setups[@]}"; do
         vscode )
             sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
             sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-            sudo dnf check-update -y
+            sudo dnf check-update --refresh
             sudo dnf install -y code
 
             setup_vscode
