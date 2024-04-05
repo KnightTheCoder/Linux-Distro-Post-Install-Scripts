95037a9f11bbae09aaf646df7433d192b09a10df
KnightTheCoder <knight14animator@gmail.com>
2024-04-05 11:03:37 +0200

    debian: only install eza if it's not installed yet

--- distros/debian/setup.sh
+++ distros/debian/setup.sh
@@ -308,13 +308,15 @@ for app in "${setups[@]}"; do
             ;;
 
         eza )
-            sudo mkdir -p /etc/apt/keyrings
-            wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
-            echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
-            sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+            if [ ! -x /usr/bin/eza ]; then
+                sudo mkdir -p /etc/apt/keyrings
+                wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+                echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+                sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
 
-            sudo nala update
-            sudo nala install -y eza
+                sudo nala update
+                sudo nala install -y eza
+            fi
             ;;
 
         flatpak )
