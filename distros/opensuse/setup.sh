0dec6d1da9ff2eb27eb6922d4e06589cadcd668a
KnightTheCoder <knight14animator@gmail.com>
2024-04-07 16:52:24 +0200

    opensuse: vscodium obs package used

--- distros/opensuse/setup.sh
+++ distros/opensuse/setup.sh
@@ -115,7 +115,7 @@ for package in $packages; do
             ;;
 
         vscodium )
-            setups+=(vscodium)
+            opi+=(vscodium)
 
             packages=${packages//"$package"/}
             ;;
@@ -223,14 +223,6 @@ for app in "${setups[@]}"; do
 
         vscode )
             setup_vscode
-            ;;
-
-        vscodium )
-            sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
-
-            printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=gitlab.com_paulcarroty_vscodium_repo\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/zypp/repos.d/vscodium.repo
-
-            sudo zypper in -y codium
             ;;
 
         hacknerd )
