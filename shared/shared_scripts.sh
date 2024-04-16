7cad734a3769569567f8695ec8865d82ba875e17
KnightTheCoder <knight14animator@gmail.com>
2024-04-16 11:05:54 +0200

    shared: comment to run omf install bobthefish removed, it will only ask to exit now

--- shared/shared_scripts.sh
+++ shared/shared_scripts.sh
@@ -98,11 +98,10 @@ function setup_fish() {
     if [ -d "$HOME/.local/share/omf" ]; then
         echo -e "${YELLOW}oh my fish is already installed!${NC}"
     else
-        echo -e "${YELLOW}Please run 'omf install bobthefish' and exit from fish once it's done so the install can continue${NC}"
+        echo -e "${YELLOW}Please run 'exit' to exit from fish and install the bobthefish theme${NC}"
         curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+        fish "../../shared/setup.fish"
     fi
-
-    fish "../../shared/setup.fish"
 
     echo -e "${GREEN}Copying fish config...${NC}"
 
