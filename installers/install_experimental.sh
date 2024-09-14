4f4bf4c4821d6086db43f9a9b3df882fd8f15a61
KnightTheCoder <90042747+KnightTheCoder@users.noreply.github.com>
2024-09-14 11:33:26 +0200

    installer: delete previous installers

--- installers/install_experimental.sh
+++ installers/install_experimental.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+rm -rf "$(xdg-user-dir DOWNLOAD)/post-install-scripts.zip" "$(xdg-user-dir DOWNLOAD)/Linux-Distro-Post-Install-Scripts-maintanence"
+
 wget -O "$(xdg-user-dir DOWNLOAD)/post-install-scripts.zip" "https://github.com/KnightTheCoder/Linux-Distro-Post-Install-Scripts/archive/refs/heads/maintanence.zip"
 cd "$(xdg-user-dir DOWNLOAD)" && unzip post-install-scripts.zip && cd Linux-Distro-Post-Install-Scripts-maintanence
 bash ./post_install.sh
