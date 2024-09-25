0f1acb983ea42eeb7031afcacde6fcc35f5b620f
KnightTheCoder <90042747+KnightTheCoder@users.noreply.github.com>
2024-09-25 14:30:47 +0200

    fedora: docker don't remove old versions

--- distros/fedora/setup.sh
+++ distros/fedora/setup.sh
@@ -306,17 +306,6 @@ for app in "${setups[@]}"; do
             ;;
 
         docker )
-            sudo dnf remove -y docker \
-                  docker-client \
-                  docker-client-latest \
-                  docker-common \
-                  docker-latest \
-                  docker-latest-logrotate \
-                  docker-logrotate \
-                  docker-selinux \
-                  docker-engine-selinux \
-                  docker-engine
-
             sudo dnf -y install dnf-plugins-core
             sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo -y
 
