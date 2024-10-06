52450057c747170b17e337c21b5cb248866de39f
KnightTheCoder <90042747+KnightTheCoder@users.noreply.github.com>
2024-10-06 16:05:29 +0200

    fedora: remove extra vivaldi repo

--- distros/fedora/setup.sh
+++ distros/fedora/setup.sh
@@ -331,6 +331,8 @@ function main() {
             sudo dnf config-manager --add-repo https://repo.vivaldi.com/archive/vivaldi-fedora.repo
 
             sudo dnf install -y vivaldi-stable
+
+            sudo rm -fv /etc/yum.repos.d/vivaldi.repo
             ;;
 
         brave)
