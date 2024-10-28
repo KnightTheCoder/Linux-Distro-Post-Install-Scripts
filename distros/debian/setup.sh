0c623ad866ef90577de7b52c68915c363087bb3f
KnightTheCoder <90042747+KnightTheCoder@users.noreply.github.com>
2024-10-28 10:33:46 +0100

    all: updated steam devices description

--- distros/debian/setup.sh
+++ distros/debian/setup.sh
@@ -21,7 +21,7 @@ function main() {
             "wine" "Wine" OFF \
             "gaming-overlay" "Gaming overlay" OFF \
             "steam" "Steam" OFF \
-            "steam-devices" "Steam devices (for controllers when using the steam flatpak)" OFF \
+            "steam-devices" "Steam devices (for the steam flatpak)" OFF \
             "itch" "Itch desktop app" OFF \
             "heroic" "Heroic Games Launcher" OFF \
             "firefox" "Firefox web browser" ON \
--- distros/opensuse/setup.sh
+++ distros/opensuse/setup.sh
@@ -50,7 +50,7 @@ function main() {
             "wine" "Wine" OFF \
             "gaming-overlay" "Gaming overlay" OFF \
             "steam" "Steam" OFF \
-            "steam-devices" "Steam devices (for controllers when using the steam flatpak)" OFF \
+            "steam-devices" "Steam devices (for the steam flatpak)" OFF \
             "itch" "Itch desktop app" OFF \
             "MozillaFirefox" "Firefox web browser" ON \
             "librewolf" "Librewolf web browser" OFF \
