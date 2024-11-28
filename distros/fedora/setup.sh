086e77d2b9d03b2ab5fd4a513d44c1a166e2a90b
KnightTheCoder <90042747+KnightTheCoder@users.noreply.github.com>
2024-11-28 21:39:07 +0100

    fedora: config-manager scripts switched to dnf5

--- distros/fedora/setup.sh
+++ distros/fedora/setup.sh
@@ -332,7 +332,7 @@ function main() {
 
         vivaldi)
             sudo dnf5 install dnf-utils -y
-            sudo dnf4 config-manager --add-repo https://repo.vivaldi.com/archive/vivaldi-fedora.repo
+            sudo dnf5 config-manager addrepo --from-repofile=https://repo.vivaldi.com/archive/vivaldi-fedora.repo
 
             sudo dnf5 install -y vivaldi-stable
 
@@ -340,7 +340,7 @@ function main() {
             ;;
 
         brave)
-            sudo dnf4 config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+            sudo dnf5 config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 
             sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 
@@ -378,7 +378,7 @@ function main() {
             ;;
 
         docker)
-            sudo dnf4 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+            sudo dnf5 config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
 
             sudo dnf5 install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
             ;;
