cbfa4fd9972cab45db0059669159bc17bed3fd8c
KnightTheCoder <90042747+KnightTheCoder@users.noreply.github.com>
2025-03-07 20:06:37 +0100

    fedora: added appstream-data so discover can show rpmfusion programs

--- distros/fedora/setup.sh
+++ distros/fedora/setup.sh
@@ -73,7 +73,7 @@ function main() {
             3>&1 1>&2 2>&3
     )
 
-    packages+=" neovim eza bat curl wget cabextract xorg-x11-font-utils fontconfig p7zip p7zip-plugins unrar git dnf-plugins-core ffmpeg-libs libva libva-utils openh264 gstreamer1-plugin-openh264 mozilla-openh264"
+    packages+=" neovim eza bat curl wget cabextract xorg-x11-font-utils fontconfig p7zip p7zip-plugins unrar git dnf-plugins-core ffmpeg-libs libva libva-utils openh264 gstreamer1-plugin-openh264 mozilla-openh264 \*appstream-data"
 
     local shells
     shells=$(choose_shells)
