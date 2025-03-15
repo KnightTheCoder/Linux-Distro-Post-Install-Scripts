9b6322380a61bdea7248ec00588f825271fe2301
KnightTheCoder <90042747+KnightTheCoder@users.noreply.github.com>
2025-03-15 16:00:24 +0100

    fedora: removed extra spaces

--- distros/fedora/setup.sh
+++ distros/fedora/setup.sh
@@ -452,7 +452,7 @@ function main() {
             ;;
 
         qemu)
-            echo -e "${GREEN} Installing virtio-win drivers for windows...${NC}"
+            echo -e "${GREEN}Installing virtio-win drivers for windows...${NC}"
             echo -e "${GREEN}Drivers can be found in ${YELLOW}/usr/share/virtio-win/${GREEN} after install is finished${NC}"
 
             sudo wget https://fedorapeople.org/groups/virt/virtio-win/virtio-win.repo \
