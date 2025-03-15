5e41d35bcdad187e51f96a9e9fc3072b32be773b
KnightTheCoder <90042747+KnightTheCoder@users.noreply.github.com>
2025-03-15 12:13:13 +0100

    fedora: added helpful message for virtio-win driver location

--- distros/fedora/setup.sh
+++ distros/fedora/setup.sh
@@ -452,6 +452,9 @@ function main() {
             ;;
 
         qemu)
+            echo -e "${GREEN} Installing virtio-win drivers for windows...${NC}"
+            echo -e "${GREEN}Drivers can be found in ${YELLOW}/usr/share/virtio-win/${GREEN} after install is finished${NC}"
+
             sudo wget https://fedorapeople.org/groups/virt/virtio-win/virtio-win.repo \
                 -O /etc/yum.repos.d/virtio-win.repo
 
