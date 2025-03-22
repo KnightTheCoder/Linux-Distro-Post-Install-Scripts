1424968be322d2941274fec5d012bf85c23289c1
KnightTheCoder <90042747+KnightTheCoder@users.noreply.github.com>
2025-03-22 20:45:31 +0100

    fedora: fixed grepping file

--- distros/fedora/setup.sh
+++ distros/fedora/setup.sh
@@ -325,7 +325,7 @@ function main() {
     fi
 
     echo -e "${GREEN}Increasing the inotify watch count...${NC}"
-    if grep -iq fs.inotify.max_user_watches=10000000 /etc/sysctl.conf || grep -iq fs.inotify.max_user_instances = 256 /etc/sysctl.conf; then
+    if grep -iq fs.inotify.max_user_watches=10000000 /etc/sysctl.conf || grep -iq "fs.inotify.max_user_instances = 256" /etc/sysctl.conf; then
         echo -e "${YELLOW}inotify watch count already modified!${NC}"
     else
         printf "\nfs.inotify.max_user_watches=10000000\nfs.inotify.max_user_instances = 256\n" | sudo tee -a /etc/sysctl.conf
