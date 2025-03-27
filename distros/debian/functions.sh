6e367f2355851749a2755b961b22380940699c9d
KnightTheCoder <90042747+KnightTheCoder@users.noreply.github.com>
2025-03-27 11:36:01 +0100

    debian, fedora: get_remove_discover space added so it's a seperate package

--- distros/debian/functions.sh
+++ distros/debian/functions.sh
@@ -375,7 +375,7 @@ function get_remove_discover() {
     local -n packages_to_remove_list=$1
 
     if [[ -x $(command -v plasma-discover) ]] && whiptail --title "Remove discover" --yesno "Would you like to remove discover?" --defaultno 0 0; then
-        packages_to_remove_list+="plasma-discover"
+        packages_to_remove_list+=" plasma-discover"
     fi
 }
 
