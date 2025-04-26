4741e0f5e4fc46733616143ed23daf629047f3da
KnightTheCoder <90042747+KnightTheCoder@users.noreply.github.com>
2025-04-26 20:24:06 +0200

    fedora: dnf swap will automatically run now

--- distros/fedora/functions.sh
+++ distros/fedora/functions.sh
@@ -393,14 +393,14 @@ function add_rpm_fusion_repos() {
 #######################################
 function swap_free_to_rpm_fusion_packages() {
     # For Intel GPUs
-    sudo dnf swap libva-intel-media-driver intel-media-driver --allowerasing
-    sudo dnf install libva-intel-driver
+    sudo dnf swap libva-intel-media-driver intel-media-driver --allowerasing -y
+    sudo dnf install libva-intel-driver -y
 
     # For AMD GPUs
-    sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld
-    sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
-    sudo dnf swap mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686
-    sudo dnf swap mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686
+    sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld -y
+    sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld -y
+    sudo dnf swap mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686 -y
+    sudo dnf swap mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686 -y
 
     sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
 }
