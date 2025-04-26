2b8c318b7f27d49c27528f9cfc2c36dcf0bc152c
KnightTheCoder <90042747+KnightTheCoder@users.noreply.github.com>
2025-04-26 19:39:46 +0200

    fedora: swapped correct drivers for intel cpus

--- distros/fedora/functions.sh
+++ distros/fedora/functions.sh
@@ -392,12 +392,17 @@ function add_rpm_fusion_repos() {
 #   None
 #######################################
 function swap_free_to_rpm_fusion_packages() {
+    # For Intel GPUs
+    sudo dnf swap libva-intel-media-driver intel-media-driver --allowerasing
+    sudo dnf install libva-intel-driver
+
+    # For AMD GPUs
     sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld
     sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
     sudo dnf swap mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686
     sudo dnf swap mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686
 
-    sudo dnf swap 'ffmpeg-free' 'ffmpeg' --allowerasing -y
+    sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
 }
 
 #######################################
