7011e4fa89380ec5fbe220f6dd68bfeb3a690b3d
KnightTheCoder <90042747+KnightTheCoder@users.noreply.github.com>
2025-04-28 18:42:41 +0200

    shared: hack nerd fonts updated

--- shared/shared_scripts.sh
+++ shared/shared_scripts.sh
@@ -351,7 +351,7 @@ function setup_hacknerd_fonts() {
 
     echo -e "${GREEN}Installing hack nerd fonts...${NC}"
 
-    download_file Hack.zip "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Hack.zip"
+    download_file Hack.zip "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Hack.zip"
     unzip ./Hack.zip -d Hack
     mkdir -p "${hacknerdfont_directory}"
     cp -fv ./Hack/*.ttf "${hacknerdfont_directory}"
